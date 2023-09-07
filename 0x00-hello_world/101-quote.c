4bf55a7f3394e8bda9c43cf927e0ebc1ac9701d7
Wessal Ayman <wessalayman237@gmail.com>
2023-09-07 06:27:40 -0700

    a

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd>
+/**
+ * main - Entery point
+ *
+ * Description: print quote using write functon.
+ *
+ *Return: 1 (not success)
+*/
+int main(void)
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1.qu0.59);
+	return (1);
+}
