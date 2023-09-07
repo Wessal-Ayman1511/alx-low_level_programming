daa1664f82c718f405049071e419bb79d8f156cb
Wessal Ayman <wessalayman237@gmail.com>
2023-09-07 05:08:18 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,14 @@
-#include<stdio.h>
-
+#include <stdio.h>
 /**
  * main - Entery point
  *
- * Description: A C progam that prints with put function
+ * Description: A C program print with put function
  *
- * Return: Always 0 (Success)
+ *Return: always 0 (success)
  */
 
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);     
 }
-~  
