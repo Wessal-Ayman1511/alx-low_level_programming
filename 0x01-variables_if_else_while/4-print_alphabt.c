abab7001d90dd92c815977a7e715688150a2ccb9
Wessal Ayman <wessalayman237@gmail.com>
2023-09-08 04:26:33 -0700

    a

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point
+ *
+ * Description: print alphabits
+ *
+ *Return: always 0 (success)
+*/
+int main(void)
+{
+	char x = 'a';
+
+	while (x <= 'z')
+	{
+		if (x == 'q' || x == 'e')
+		{
+			x++;
+		}
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
