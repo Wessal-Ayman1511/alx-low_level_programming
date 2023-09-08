3f5491bee25625de978c32fc378e2748ff2a0f4e
Wessal Ayman <wessalayman237@gmail.com>
2023-09-08 05:11:17 -0700

    a

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point
+ *
+ * Description: print numbers
+ *
+ *Return: always 0 (success)
+*/
+int main(void)
+{
+	int x = 48;
+
+	while (x <= 57)
+	{
+		if (x == 57)
+		{
+			putchar(x);
+			break;
+		}
+		putchar(x);
+		putchar(',');
+		putchar(' ');
+		x++;
+	}
+	return (0);
+}
