8c4cdd932bbcbf6491642c29acec20b00fbef69f
Wessal Ayman <wessalayman237@gmail.com>
2023-09-12 09:42:02 -0700

    a

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include"main.h"
+
+/**
+* times_table - function to print timetable
+*
+*/
+void times_table(void)
+{
+	int n, i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(48);
+		for (j = 0; j <= 9 ; j++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			n = i * j;
+
+			if (n <= 9)
+				_putchar(' ');
+			else
+				_putchar((n / 10) + 48);
+			_putchar((n % 10) + 48);
+		}
+		_putchar('\n');
+	}
+}
