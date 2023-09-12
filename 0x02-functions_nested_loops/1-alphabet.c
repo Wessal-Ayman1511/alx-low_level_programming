a88b4400426a1bb81a4da74224f36667c4afa9f4
Wessal Ayman <wessalayman237@gmail.com>
2023-09-12 07:26:07 -0700

    a

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabets using _putchar
+ *
+*/
+void print_alphabet(void)
+{
+	int i;
+
+	for (int i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
