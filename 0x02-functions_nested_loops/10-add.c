731a10c33c331e5edf045533ba40c35c4fcf1045
Wessal Ayman <wessalayman237@gmail.com>
2023-09-12 09:52:40 -0700

    a

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+* add - add two numbers
+*
+* @a : take input
+* @b : take input
+*
+* Return:  add a & b
+*
+*/
+int add(int a, int b)
+{
+	int sum = 0;
+
+	sum = a + b;
+	return (sum);
+}
