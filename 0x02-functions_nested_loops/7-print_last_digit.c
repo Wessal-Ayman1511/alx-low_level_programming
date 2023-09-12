cdc060a95c847383554f0bbd50ddea9a7d25cd52
Wessal Ayman <wessalayman237@gmail.com>
2023-09-12 08:35:48 -0700

    a

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+* print_last_digit  - function to print last digit
+*
+* @n: chack imput of function
+*
+* Return: return 0 always  (success)
+*
+*/
+int print_last_digit(int n)
+{
+	_putchar(n % 10);
+	return (0);
+}
+
