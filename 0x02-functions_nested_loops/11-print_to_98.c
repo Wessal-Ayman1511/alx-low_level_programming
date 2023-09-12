ec1e0ee39509e37abcff603dbe9765fd83f012b0
Wessal Ayman <wessalayman237@gmail.com>
2023-09-12 10:01:34 -0700

    a

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+* print_to_98 - unction that prints all natural numbers from n to 98
+*
+* @n: take input
+*/
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i <= 98; i++)
+	{
+		printf("%d, ", i);
+	}
+	putchar('\n');
+}
