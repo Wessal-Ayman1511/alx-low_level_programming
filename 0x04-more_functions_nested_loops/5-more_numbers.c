0b66b55cf03888463e9cc2622a8ee09e67cc91df
Wessal Ayman <wessalayman237@gmail.com>
2023-09-14 06:25:31 -0700

    a

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,15 +6,25 @@
 */
 void more_numbers(void)
 {
-	int i, j;
+	int i, j, num;
 
 	for (i = 1; i <= 10; i++)
 	{
-		for (j = 0; j < 15; j++)
+		for (j = 0; j <= 14; j++)
 		{
-			_putchar(j + '0');
+			num = j;
+			if ( j > 9)
+				{
+					_putchar(1 + '0');
+					num = j % 10;
+				}
+			else
+			{
+				_putchar(num + 48);
+			}
+
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
+
 }
