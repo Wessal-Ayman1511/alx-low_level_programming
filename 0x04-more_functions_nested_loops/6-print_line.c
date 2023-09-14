5d841cdf8c1a5e6efa214ebf250a9b41a46c87e0
Wessal Ayman <wessalayman237@gmail.com>
2023-09-14 06:54:53 -0700

    a

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+* print_line - print lines using _putchar
+*
+* @n: input
+*
+*/
+void print_line(int n)
+{
+	if (n >= 1)
+	{
+		for (i = 1; i <= n ; i++)
+			_putchar('_');
+	}
+	 _putchar('\n');
+}
