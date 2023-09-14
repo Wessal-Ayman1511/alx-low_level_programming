c9eb58bce8054d5939aff1f39a1f0ebe66a488af
Wessal Ayman <wessalayman237@gmail.com>
2023-09-14 07:44:08 -0700

    a

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,14 +14,12 @@ void print_diagonal(int n)
 	{
 		for (i = 0; i < n; i++)
 		{
-			for (j = 0; j < n; j++)
+			for (j = 0; j < i; j++)
 			{
-				if (i == j)
-					_putchar(92);
-				else
-					_putchar(' ');
+				_putchar(' ');
 			}
-			_putchar('\n');
+				_putchar(92);
+				_putchar('\n');
 		}
 	}
 	_putchar('\n');
