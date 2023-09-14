fa7c7754d3225cddfa0e50896e83a38297382553
Wessal Ayman <wessalayman237@gmail.com>
2023-09-14 05:37:34 -0700

    a

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+* print_numbers - print numbers using _putchar
+*
+*/
+void print_numbers(void);
+{
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
