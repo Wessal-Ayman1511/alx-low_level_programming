512fb808d4007c430b81fbcc982dc4bd93ef7c73
Wessal Ayman <wessalayman237@gmail.com>
2023-09-18 07:30:03 -0700

    a

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,9 +8,12 @@
 */
 void puts_half(char *str)
 {
-	int i = 0, len = strlen(str);
+	int i;
 
-	for (i = len / 2; str[i] != '\0' ; i++)
+	for (i = 0; str[i] != '\0' ; i++)
+		;
+
+	for (i /= 2; str[i] != '\0' ; i++)
 		_putchar(str[i]);
 	_putchar('\n');
 }
