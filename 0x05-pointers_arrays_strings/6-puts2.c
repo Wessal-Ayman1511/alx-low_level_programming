b0b2287dda0ddc2be68ce99a71f2daa0d8bcad75
Wessal Ayman <wessalayman237@gmail.com>
2023-09-18 06:18:32 -0700

    a

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include"main.h"
+#include<string.h>
+/**
+* puts2- function that prints every other character of a string
+*
+* @str: input
+*
+*/
+void puts2(char *str)
+{
+	int i = 0, len = strlen(s);
+
+	for (i = 0  ; i < len  ; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
