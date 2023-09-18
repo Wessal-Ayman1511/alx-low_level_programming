5fc11076c4a4d42d03fa56ad5ab64ace9802be32
Wessal Ayman <wessalayman237@gmail.com>
2023-09-18 05:41:46 -0700

    a

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include"main.h"
+#include<string.h>
+/**
+* print_rev- function that reverse  string.
+*
+* @s: input
+*
+*/
+void print_rev(char *s)
+{
+	int i = 0, len = strlen(s);
+
+	for (i = len - 1 ; i >= 0 ; i--)
+	{
+		_putchar(p[i]);
+	}
+	_putchar('\n');
+}
