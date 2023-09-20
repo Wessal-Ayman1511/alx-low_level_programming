e0c52ca00ab375748077172f21ba19a485e5d402
Wessal Ayman <wessalayman237@gmail.com>
2023-09-20 00:49:49 -0700

    a

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include"main.h"
+#include<string.h>
+/**
+*_strcat- function that concatenates two strings.
+*
+* @dest: input
+*
+* @src: input
+*
+* Return: return(poiter)
+*/
+char *_strcat(char *dest, char *src);
+{
+	int *p;
+
+	p = strcat(dest, src);
+	return (p);
+}
