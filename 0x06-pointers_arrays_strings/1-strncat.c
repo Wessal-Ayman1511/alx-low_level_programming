1c8c7f839c2aa4c06cbacc6dfc5c42c0978442e5
Wessal Ayman <wessalayman237@gmail.com>
2023-09-22 12:31:42 -0700

    a

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -15,7 +15,7 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int len = strlen(dest), c = 0;
 
-	for (c = 0 ; c < n && src[i] != '\0' ; c++)
+	for (c = 0 ; c < n && src[c] != '\0' ; c++)
 		dest[len + c] = src[c];
 	dest[len + c] = '\0';
 	return (dest);
