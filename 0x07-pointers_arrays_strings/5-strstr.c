e4f44d05b8775c90925e3220491cc8089cac6397
Wessal Ayman <wessalayman237@gmail.com>
2023-09-25 09:03:40 -0700

    a

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include"main.h"
+#include<string.h>
+/**
+**_strstr- function that locates a substring.
+*
+* @needle: input
+*
+* @haystack: input
+*
+* Return: return(pointer)
+*/
+char *_strstr(char *haystack, char *needle);
+{
+	while (*haystack != '\0')
+	{
+		char *h = haystack;
+		char *n = needle;
+
+		while (*h == *n && *n != '\0')
+		{
+			h++;
+			n++;
+		}
+		if (*n == '\0')
+			return (haystack);
+		haystack++;
+	}
+	return (0);
+}
