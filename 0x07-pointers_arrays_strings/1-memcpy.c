1549d39903eb6d4034827ffeec46e8a720b70b2b
Wessal Ayman <wessalayman237@gmail.com>
2023-09-25 05:59:51 -0700

    a

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -13,13 +13,12 @@
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	 int i;
+	unsigned  int i;
 
-	for (i = 0; s[i] != '\0' ; i++)
+	for (i = 0; i < n ; i++)
 	{
 		dest[i] = src[i];
 	}
-	dest[i] = '\0';
 	return (dest);
 }
 
