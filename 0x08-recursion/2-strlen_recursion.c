2929e94e916fc7b328f2230caf1d69721c829109
Wessal Ayman <wessalayman237@gmail.com>
2023-09-28 05:24:40 -0700

    a

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -11,9 +11,12 @@ int _strlen_recursion(char *s)
 {
 	int c = 0;
 
-	if (*s == '\0')
+	if (*s != '\0')
+	{
+		c++;
+		_strlen_recursion(s + 1)
+	}
+	else
 		return (c);
-	c++;
-	_strlen_recursion(s + 1);
 
 }
