0f10eb661fa050de4c312eb6d406c5bf1776ac56
Wessal Ayman <wessalayman237@gmail.com>
2023-09-28 07:47:29 -0700

    a

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -29,8 +29,11 @@ int prime(int n, int i)
 {
 	if (i == 1)
 		return (1);
-	else if (n % i == 0)
-		return (0);
 	else
+	{	
+		if (n % i == 0)
+		return (0);
+		else
 		return (prime(n, i--));
+	}
 }
