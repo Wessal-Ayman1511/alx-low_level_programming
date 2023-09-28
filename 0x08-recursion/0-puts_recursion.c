492bdcba7bfddf45374117b8c0ebd395a585049e
Wessal Ayman <wessalayman237@gmail.com>
2023-09-28 04:20:01 -0700

    a

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include"main.h"
+#include<string.h>
+/**
+* _puts_recursion- recursive function that  print string
+*
+* @s: input
+*
+* Return: always zer
+*/
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		return ('\n');
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
