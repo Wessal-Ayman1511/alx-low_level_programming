0cd33c140195a2c14a16fe029ad4d7f97f09c19b
Wessal Ayman <wessalayman237@gmail.com>
2023-09-28 06:34:27 -0700

    a

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include"main.h"
+#include<string.h>
+/**
+* _sqrt_recursion- recursive function that calc sqr
+*
+* @n: input
+*
+* Return: value
+*/
+int sqr(int n, int val);
+int _sqrt_recursion(int n);
+{
+	return (sqr(n, 1));
+}
+/**
+ * sqr- function to calc sqr
+ *
+ * @n: input
+ *
+ * @val: input
+ *
+ * Return: value
+ */
+int sqr(int n, int val)
+{
+	if (val * val == n)
+		return (val);
+	else if (val * val < n)
+		return (sqr(n, val - 1));
+	else
+i		return (-1);
+}
+
