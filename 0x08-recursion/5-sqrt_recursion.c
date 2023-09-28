89f7d797c57db9b397e6e8e8ca4fbb554aeb1773
Wessal Ayman <wessalayman237@gmail.com>
2023-09-28 06:40:28 -0700

    a

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,12 +5,14 @@
 *
 * @n: input
 *
+* @val: input
+*
 * Return: value
 */
 int sqr(int n, int val);
 int _sqrt_recursion(int n)
 {
-	return (sqr(n, 1));
+return (sqr(n, 1));
 }
 /**
  * sqr- function to calc sqr
@@ -23,10 +25,10 @@ int _sqrt_recursion(int n)
  */
 int sqr(int n, int val)
 {
-	if (val * val == n)
-		return (val);
-	else if (val * val < n)
-		return (sqr(n, val - 1));
-	else
+if (val * val == n)
+	return (val);
+else if (val * val < n)
+	return (sqr(n, val + 1));
+else
 		return (-1);
 }
