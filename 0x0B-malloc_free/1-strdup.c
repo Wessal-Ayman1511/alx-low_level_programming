b2aaf0414a452d5aa21c8d60442e7b3cbe86ff99
Wessal Ayman <wessalayman237@gmail.com>
2023-10-03 11:39:16 -0700

    a

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * *_strdup- function that  newly allocated space in memory
+ * @c: input
+ * Return: Nothing.
+ */
+char *_strdup(char *str)
+{
+	int i = 0, len = strlen(str);
+	char *ptr = malloc(len);
+
+	if (ptr == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < len ; i++)
+		{
+			ptr[i] = str[i];
+		}
+	}
+	return (ptr);
+}
