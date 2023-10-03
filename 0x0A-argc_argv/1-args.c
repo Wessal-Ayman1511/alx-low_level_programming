ff3c9e024c5bd64dfce55e1f714024f3461e2daa
Wessal Ayman <wessalayman237@gmail.com>
2023-10-03 04:33:14 -0700

    a

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - entery point
+*
+* @argc: input
+*
+* @argv: input
+*
+* Return: always zero
+*
+*/
+int main(int argc, char const *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
