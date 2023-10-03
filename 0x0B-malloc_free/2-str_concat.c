fad9d65dae4201a3143276e2ee1ec9f770e2d462
Wessal Ayman <wessalayman237@gmail.com>
2023-10-03 12:53:08 -0700

    a

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,7 +10,7 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *ptr = malloc(strlen(s2) + strlen(s1) + 1);
-	int i, j = 0;
+	int i, j = 0,l1 = strlen[s1], l2 = strlen[s2];
 
 	if (ptr == NULL)
 	{
@@ -18,12 +18,12 @@ char *str_concat(char *s1, char *s2)
 	}
 	else
 	{
-		for (i = strlen(s1) ; i < strlen(s1) + strlen(s2) ; i++)
+		for (i = l1 ; i < l1 + l2 ; i++)
 		{
 			s1[i] = s2[j];
 			j++;
 		}
-		s1[strlen(s2) + strlen(s1)] = '\0';
+		s1[l1 + l2] = '\0';
 		return (s1);
 	}
 }
