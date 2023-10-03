bd30c0f8e700b4c085bfe210d8a3e40bbb85f91f
Wessal Ayman <wessalayman237@gmail.com>
2023-10-03 04:26:04 -0700

    a

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+* main - entery point
+*
+* @argc: input
+*
+* @argv: input
+*
+* Return: always zero
+*
+*/
+int main(int argc, char const *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
