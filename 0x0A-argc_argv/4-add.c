407df056dcd0f9ff0f9ac11be616530c9a8a99fd
Wessal Ayman <wessalayman237@gmail.com>
2023-10-03 05:17:49 -0700

    a

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,7 +10,7 @@
 * Return: always zero
 *
 */
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
 	int sum = 0;
 	char *c;
@@ -18,10 +18,8 @@ int main(int argc, char const *argv[])
 	while (--argc)
 	{
 		for (c = argv[argc] ; *c ; c++)
-		{
 			if (*c < '0' || *c > '9')
 				return (printf("Error\n"), 1);
-		}
 		sum += atoi(argv[argc]);
 	}
 	printf("%d\n", sum);
