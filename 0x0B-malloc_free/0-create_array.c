cd6e003d63ac8c0195846d51eaaabef9012df323
Wessal Ayman <wessalayman237@gmail.com>
2023-10-03 11:23:40 -0700

    a

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * *create_array - function that create array
+ * @c: input
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+char *create_array(unsigned int size, char c)
+{
+	int i = 0;
+	char *ptr =  malloc(size);
+
+	if (size == 0 || ptr == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0 ; i < size ; i++)
+		{
+			ptr[i] = c;
+		}
+	}
+	return (ptr);
+}
