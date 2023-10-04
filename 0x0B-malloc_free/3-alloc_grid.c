a1d15e53e8f83418078cc83b44168fa583fb1df3
Wessal Ayman <wessalayman237@gmail.com>
2023-10-04 03:02:38 -0700

    a

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -7,7 +7,7 @@
  */
 int **alloc_grid(int width, int height)
 {
-	char **p = calloc((width * height) * sizeof(int));
+	char **p = calloc((width * height), sizeof(int));
 
 	if (p == NULL || width <= 0 || height <= 0)
 	{
