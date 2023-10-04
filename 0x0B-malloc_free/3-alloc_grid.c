19a7b39d1e33ae9fbbe8f4e7a2b4cbeeb3a67e4a
Wessal Ayman <wessalayman237@gmail.com>
2023-10-04 04:45:23 -0700

    a

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,7 +9,7 @@ int **alloc_grid(int width, int height)
 {
 	int i, j, **p;
 
-	p = malloc(height *  sizeof(int));
+	p = malloc(height *  sizeof(*p));
 	if (p == NULL || width <= 0 || height <= 0)
 	{
 		return (NULL);
@@ -18,19 +18,19 @@ int **alloc_grid(int width, int height)
 	{
 		for (i = 0 ; i < height ; i++)
 		{
-			p[i] = malloc(width *  sizeof(int));
+			p[i] = malloc(width *  sizeof(**p));
 			if (p[i] == 0)
 			{
 				while (i--)
 					free(p[i]);
 				free(p);
-				return (0);
+				return (NULL);
 			}
 			for (j = 0 ; j < width ; j++)
 			{
 				p[i][j] = 0;
 			}
 		}
-		return (p);
 	}
+	return (p);
 }
