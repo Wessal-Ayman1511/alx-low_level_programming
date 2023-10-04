1f4e243c7745ddf8d74e9981f80e17c893bbd039
Wessal Ayman <wessalayman237@gmail.com>
2023-10-04 03:01:43 -0700

    a

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * **alloc_grid- function allocategrid with zeros
+ * @width: input
+ * @height: input
+ * Return: pointer
+ */
+int **alloc_grid(int width, int height)
+{
+	char **p = calloc((width * height) * sizeof(int));
+
+	if (p == NULL || width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	else
+		return (p);
+}
