8b352bb36ea5481162a575e492279abdf8dafaa9
Wessal Ayman <wessalayman237@gmail.com>
2023-10-07 11:32:09 -0700

    a

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,18 @@
 #include "main.h"
 /**
+ * _strlen- function that return length
+ * @s: input
+ * Return: length
+*/
+int _strlen(char *s)
+{
+	int i = 0;
+
+	for (; s[i] != '\0' ; i++)
+	;
+	return (i);
+}
+/**
  * *string_nconcat- funcrion that concat two strings
  * @s1: input
  * @s2: input
@@ -7,3 +20,28 @@
  * Return: pointer
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int i = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	int l1 = _strlen(s1);
+	int l2 = _strlen(s2);
+	char *p = malloc(sizeof(char) * (l1 + n + 1));
+
+	if (p == 0)
+	{
+		return (NULL);
+	}
+	for (i = 0 ; i < l1 + n ; i++)
+	{
+		if (i < l1)
+			p[i] = s1[i];
+		else if (i < l1 + n)
+			p[1] = s2[i - l1];
+	}
+	return (p);
+}
+
