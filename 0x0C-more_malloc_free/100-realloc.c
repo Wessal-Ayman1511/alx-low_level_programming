47b0a48d923d6fcbea811b2589a6ac653f48e795
Wessal Ayman <wessalayman237@gmail.com>
2023-10-08 11:25:01 -0700

    a

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * *_realloc- funcrion that reallocates memory
+ * @ptr:input
+ * @old_size: input
+ * @new_size: input
+ * Return: pointer
+*/
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	unsigned int i;
+	char *p;
+
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (ptr == NULL)
+	{
+		p = malloc(new_size);
+		if (p == NULL)
+		{
+			return (NULL);
+		}
+		return (p);
+	}
+	if (new_size > old_size)
+	{
+		p = malloc(new_size);
+		if (p == NULL)
+		{
+			return (NULL);
+		}
+		for (i = 0 ; i < old_size && i < new_size ; i++)
+		{
+			p[i] = ptr[i];
+		}
+		free(ptr);
+	}
+	return (p);
+}
