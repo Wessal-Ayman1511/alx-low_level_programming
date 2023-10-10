6f90cfd03080b35aaa0cd8202bbb4ba53873f54e
Wessal Ayman <wessalayman237@gmail.com>
2023-10-10 10:38:23 -0700

    a

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog- structure for dog
+ * @name:element
+ * @age:element
+ * @owner:element
+ * Description: struct for dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
