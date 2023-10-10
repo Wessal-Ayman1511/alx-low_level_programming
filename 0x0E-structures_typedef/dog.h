b1e1b9a6012748c25cbfab5ec93dafeebff5ebb9
Wessal Ayman <wessalayman237@gmail.com>
2023-10-10 09:30:04 -0700

    a

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG-H
+
+/**
+ * struct dog - a dog struct
+ * @name: member
+ * @age: member
+ * @owner: member
+ * Discription:Poppy love kitty
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};      
+#endif
