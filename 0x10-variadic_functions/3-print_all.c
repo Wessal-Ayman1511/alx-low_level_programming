474c97816160f31a09a287742e82d04c27fc6db4
Wessal Ayman <wessalayman237@gmail.com>
2023-10-12 13:21:01 -0700

    a

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,4 +1,5 @@
 #include "variadic_functions.h"
+
 /**
  * print_all- function that print
  * @format: iinput
@@ -7,38 +8,46 @@
 void print_all(const char * const format, ...)
 {
 	int i = 0, flag;
-	char *s;
+	char *st;
 	va_list ptr;
 
 	va_start(ptr, format);
 	while (format != NULL && format[i] != '\0')
 	{
-		if (format[i] == 'i')
+		switch (format[i])
 		{
-			printf("%d", va_arg(ptr, int));
-			flag = 0;
-		}
-		if (format[i] == 'f')
-		{
-			printf("%f", va_arg(ptr, double));
-			flag = 0;
-		}
-		if (format[i] == 'c')
-		{
-			printf("%c", va_arg(ptr, int));
-			flag = 0;
-		}
-		if (format[i] == 's')
-		{
-			s = va_arg(ptr, char *);
-			if (s == NULL)
-				s = "(nil)";
-			printf("%s", s);
-		}
-		if (format[i] != 'i' && format[i] != 'f' &&
-		format[i] != 'c' && format[i] != 's')
-		{
-			flag = 1;
+			case 'i':
+			{
+				printf("%d", va_arg(ptr, int));
+				flag = 0;
+				break;
+			}
+			case 'f':
+			{
+				printf("%f", va_arg(ptr, double));
+				flag = 0;
+				break;
+			}
+			case 'c':
+			{
+				printf("%c", va_arg(ptr, int));
+				flag = 0;
+				break;
+			}
+			case 's':
+			{
+				st = va_arg(ptr, char *);
+				if (st == NULL)
+					st = "(nil)";
+				printf("%s", st);
+				flag = 0;
+				break;
+			}
+			default:
+			{
+				flag = 1;
+				break;
+			}
 		}
 		if (format[i + 1] != '\0' && flag == 0)
 			printf(", ");
