805b458103b048279b28410ea3cdabba9c27af4c
Wessal Ayman <wessalayman237@gmail.com>
2023-10-12 11:40:35 -0700

    a

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,39 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers- function that print numbers
+ * @separator: input
+ * @n: input
+ * Return: void
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int i;
+	va_list ptr;
+
+	if (!n)
+	{
+		printf("\n");
+		return;
+	}
+	va_start(ptr, n);
+	for (i = 0; i < n ; i++)
+	{
+		if (separator == NULL)
+		{
+			for (i = 0; i < n ; i++)
+				printf("%d", va_arg(ptr, int));
+			printf("\n");
+		}
+		else
+		{
+			for (i = 1; i <= n; i++)
+			{
+				if (i == n - 1)
+					printf("%d", va_arg(ptr, int));
+				else
+					printf("%d%s", va_arg(ptr, int), separator);
+			}
+			printf("\n");
+		}
+	}
+}
