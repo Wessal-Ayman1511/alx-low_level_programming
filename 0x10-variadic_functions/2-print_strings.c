ea767271addd82f4293eb45fa3e462628e488734
Wessal Ayman <wessalayman237@gmail.com>
2023-10-12 12:03:18 -0700

    a

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+/**
+ * print_strings- function that print string
+ * @n: input
+ * @separator: input
+ * Return: void
+*/
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	int count = n;
+	char *s;
+	va_list ptr;
+
+	if (!n)
+	{
+		printf("\n");
+		return;
+	}
+	while (count--)
+	{
+		printf("%s%s", (s = va_arg(ptr, char *)) ? str : "(nil)",
+				count ? (separator ? separator : "") : "\n");
+	}
+	va_end(ptr);
+}
