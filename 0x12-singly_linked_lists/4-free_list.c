710e3a4619addfd80c3f42116195dd134a4fc301
Wessal Ayman <wessalayman237@gmail.com>
2023-10-19 12:59:52 -0700

    a

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * free_list- function free list
+ * @head: input
+ * Return: void
+*/
+void free_list(list_t *head)
+{
+	list_t *node1, *node2;
+
+	if(!head)
+		return;
+	node1 = head;
+	while (node1)
+	{
+		node2 = node1->next;
+		free(node1->str);
+		free(node1);
+		node1 = node2;
+	}
+}
