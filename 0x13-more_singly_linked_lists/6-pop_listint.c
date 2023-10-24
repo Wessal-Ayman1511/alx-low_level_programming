22abdcb569cc64ed10c9d43e02e5b44e4aec570b
Wessal Ayman <wessalayman237@gmail.com>
2023-10-24 10:58:36 -0700

    a

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * pop_listint- function that delete first node
+ * @head: input
+ * Return: n-data
+*/
+int pop_listint(listint_t **head)
+{
+	listint_t *tmp;
+	int n;
+	if (!*head || !head)
+	{
+		return (0);
+	}
+	else
+	{
+		tmp = *head;
+		*head = (*head)->next;
+		n = tmp->n;
+		free(tmp);
+	}
+	return (n);
+}
