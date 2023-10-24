d9dfcc238af698bfd11ca4470f3f230f731c2fed
Wessal Ayman <wessalayman237@gmail.com>
2023-10-24 11:31:33 -0700

    a

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -7,11 +7,12 @@
 */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *ptr = head;
+	listint_t *ptr;
 	unsigned int c = 0;
 
 	if (!head)
 		return (NULL);
+	ptr = head;
 	while (c < index)
 	{
 		ptr = ptr->next;
