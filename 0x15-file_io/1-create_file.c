48059e56efa113a7798fc9e80713d5e1b8a5c496
Wessal Ayman <wessalayman237@gmail.com>
2023-11-02 02:11:53 -0700

    a

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,9 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	f = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	f = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (f == -1)
+		return (-1);
 	if (length)
 		num_bytes = write(f, text_content, length);
 	close(f);
