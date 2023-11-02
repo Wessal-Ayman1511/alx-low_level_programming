bf14560971b9d49d336517afd9e556db26c2451b
Wessal Ayman <wessalayman237@gmail.com>
2023-11-02 02:17:05 -0700

    a

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,4 +7,16 @@
 */
 int append_text_to_file(const char *filename, char *text_content)
 {
+	int f;
+	ssize_t num_bytes = 0, length = strlen(text_content);
+
+	if (!filename)
+		return (-1);
+	f = open(filename, O_WRONLY | O_APPEND);
+	if (f == -1)
+		return (-1);
+	if (length)
+		num_bytes = write(f, text_content, length);
+	close(f);
+	return (num_bytes == length ? 1 : -1);
 }
