2469e6aadac0ff83ea94ab8bfa14ad25ece30161
root <root@Wessal-Ayman.localdomain>
2024-01-29 16:39:01 +0200

    a

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -12,19 +12,9 @@ int mul(int a, int b)
 }
 int div(int a, int b)
 {
-    if(b == 0)
-    {
-        printf("Error: Division by zero\n");
-        return 0;
-    }
     return a / b;
 }
 int mod(int a, int b)
 {
-    if(b == 0)
-    {
-        printf("Error: Division by zero\n");
-        return 0;
-    }
     return a % b;
 }
