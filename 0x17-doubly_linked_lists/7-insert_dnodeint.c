c5ea9b29b18b8a7b33957cc209abb8e9b1bd1959
root <root@Wessal-Ayman.localdomain>
2024-02-01 17:20:38 +0200

    a

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -28,7 +28,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (add_dnodeint_end(h, n));
 	}
 	tmp = malloc(sizeof(dlistint_t));
-	if (!tmp)
+	if (tmp == NULL)
 		return (NULL);
 
 	tmp->n = n;
