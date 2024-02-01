b0a5c634c2cb4445c377c81bd6f44d9e8afcc6cc
root <root@Wessal-Ayman.localdomain>
2024-02-01 12:46:05 +0200

    a

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -8,13 +8,10 @@ void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *ptr = NULL;
 
-	while (head->next != NULL)
+	while (head)
 	{
 		ptr = head;
 		head = head->next;
 		free(ptr);
-		ptr = NULL;
 	}
-	free(head);
-	head = NULL;
 }
