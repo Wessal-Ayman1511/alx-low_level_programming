8ea9d402d8b5d3cf9ef59bd09e2816e6173b0ec7
root <root@Wessal-Ayman.localdomain>
2024-02-01 17:41:35 +0200

    a

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+/**
+ * *delete_dnodeint_at_index- function that add node
+ * @head: input
+ * @idex: input
+ * @idx
+ * Return: num of nodes
+*/
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+    dlistint_t *ptr = *head;
+
+    if (*head == NULL)
+        return (-1);
+    for (; index != 1 ; index--)
+	{
+		if (ptr == NULL)
+			return (-1);
+		ptr = ptr->next;
+	}
+    if (ptr == *head)
+    {
+        *head = (*head)->next;
+        if (*head != NULL)
+            (*head)->prev = NULL;
+        
+    }
+    else
+    {
+        ptr->prev->next = ptr->next;
+        if (ptr->next != NULL)
+            ptr->next->prev = ptr->prev;
+    }
+    free(ptr);
+    return (1);
+}
