557325ded1f80eb300bcb1c4dcf714f250c89e47
Wessal Ayman <wessalayman237@gmail.com>
2024-02-16 15:38:36 -0800

    Wessal

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+#include<stdbool.h>
+
+/**
+ * hash_table_delete - delete table
+ * @ht: input
+ * Return: void
+*/
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *cur;
+
+	if (ht == NULL || ht->array == NULL)
+	return;
+	for (i = 0; i < ht->size; i++)
+	{
+
+		while (ht->array[i] != NULL)
+		{
+			cur = ht->array[i]->next;
+			free(ht->array[i]->value);
+			free(ht->array[i]->key);
+			ht->array = cur;
+		}
+	}
+	free(ht->array);
+	free(ht);
+
+}
