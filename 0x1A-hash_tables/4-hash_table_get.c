76ee89fc7f3cbcda31fe9df96557046d5b2f155f
Wessal Ayman <wessalayman237@gmail.com>
2024-02-16 15:07:12 -0800

    Wessal

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,13 +11,10 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *cur;
 	unsigned long int idx;
 
-	if (key == NULL || ht == NULL)
+	if (ht == NULL || key == NULL ||
+		ht->array == NULL || ht->size == 0 || strlen(key) == 0)
 		return (NULL);
 	idx = key_index((unsigned char *)key, ht->size);
-	if (ht->array[idx] == NULL)
-		return (NULL);
-	if (strcmp(ht->array[idx]->key, key) == 0)
-		return (ht->array[idx]->value);
 	cur = ht->array[idx];
 	while (cur != NULL)
 	{
